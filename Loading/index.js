6070953fd6dc1317b4ecea8d7a4321daa0e4156e
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-02-17 10:56:23 +0700

    feat: add animation logo

--- Loading/index.js
+++ Loading/index.js
@@ -1,16 +1,27 @@
-const arr = [1, 2, 3, 4, 5, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const arr = [1, 2, 3, 4, 5, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-const rs1 = arr.filter((item, index, array) => {
-    return item % 2 === 0;
-});
-console.log('🚀 ~ rs1 ~ rs1:', rs1);
+// const rs1 = arr.filter((item, index, array) => {
+//     return item % 2 === 0;
+// });
+// console.log('🚀 ~ rs1 ~ rs1:', rs1);
 
-const rs2 = arr.find((item) => {
-    return item % 2 === 0;
-});
-console.log('🚀 ~ rs2 ~ rs2:', rs2);
+// const rs2 = arr.find((item) => {
+//     return item % 2 === 0;
+// });
+// console.log('🚀 ~ rs2 ~ rs2:', rs2);
 
-const rs3 = arr.map((item) => {
-    return item * 2;
-});
-console.log('🚀 ~ rs3 ~ rs3:', rs3);
+// const rs3 = arr.map((item) => {
+//     return item * 2;
+// });
+// console.log('🚀 ~ rs3 ~ rs3:', rs3);
+
+// const returnValue = () => {
+//     const rs1 = 1;
+//     const rs2 = 2;
+//     const rs3 = 3;
+//     const rs4 = 4;
+
+//     return { rs1, rs2, rs3, rs4 };
+// };
+
+// const { rs1: newRs1 } = returnValue();
